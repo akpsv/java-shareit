d755ec269bd8580f288f676f3eed959a8906dd6f
Алексей Копосов <alexkopos@ya.ru>
2022-09-20 19:28:52 +0300

    fix: Исправить в соответствии с замечаниями

--- src/main/java/ru/practicum/shareit/error/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.error;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,6 +13,24 @@ public class ErrorHandler {
     public ErrorResponse someExceptionHandler(NotFoundException e) {
         return ErrorResponse.builder()
                 .error("Элемент не найден")
+                .message(e.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse validationExceptionHandler(MethodArgumentNotValidException e) {
+        return ErrorResponse.builder()
+                .error("Ошибка при проверке аргумента метода на соответствие ограничениям.")
+                .message(e.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse otherExceptionHandler(Throwable e) {
+        return ErrorResponse.builder()
+                .error("Ошибка на сервере")
                 .message(e.getMessage())
                 .build();
     }
